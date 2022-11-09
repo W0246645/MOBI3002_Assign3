68cf91daa6fe330aabb6f5b7f47c9b4de2901e68
Matthew Covey <90411582+W0246645@users.noreply.github.com>
2022-11-08 22:31:14 -0400

    Started to flesh out MainActivity.java

--- CruddyPizza/app/src/main/java/com/example/cruddypizza/MainActivity.java
+++ CruddyPizza/app/src/main/java/com/example/cruddypizza/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.cruddypizza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_newOrder;
+    Switch swtch_lang;
+    TextView txt_success;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_newOrder = findViewById(R.id.btn_newOrder);
+        txt_success = findViewById(R.id.txt_success);
+        swtch_lang = findViewById(R.id.sw_lang);
+        txt_success.setText("");
+
+        btn_newOrder.setOnClickListener(onNewClicked);
     }
+
+    public View.OnClickListener onNewClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, NewOrder.class);
+            startActivity(i);
+        }
+    };
 }
