62e258e65f01d1e728c465165241866bed232b57
Matthew Covey <w0246645@campus.nscc.ca>
2022-11-16 18:28:35 -0400

    Added listview to home page, creating now mostly works.

--- CruddyPizza/app/src/main/java/com/example/cruddypizza/MainActivity.java
+++ CruddyPizza/app/src/main/java/com/example/cruddypizza/MainActivity.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_newOrder;
     Switch swtch_lang;
     TextView txt_success;
+    Intent intent;
+    ArrayList<ArrayList<String>> table = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listData = new ArrayList<>();
+    ListView orderList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +31,34 @@ public class MainActivity extends AppCompatActivity {
 
         btn_newOrder = findViewById(R.id.btn_newOrder);
         txt_success = findViewById(R.id.txt_success);
-        swtch_lang = findViewById(R.id.sw_lang);
         txt_success.setText("");
+        swtch_lang = findViewById(R.id.sw_lang);
+        orderList = findViewById(R.id.orderList);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listData);
+        orderList.setAdapter(adapter);
 
         btn_newOrder.setOnClickListener(onNewClicked);
+
+        intent = getIntent();
+        if (intent != null) {
+            table = (ArrayList<ArrayList<String>>) intent.getSerializableExtra("table");
+            int orderNum = 0;
+            if (table != null) {
+                for (ArrayList<String> order : table) {
+                    String orderDetails = order.get(0) + " " + order.get(1);
+                    listData.add(orderDetails);
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
 
     public View.OnClickListener onNewClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent i = new Intent(MainActivity.this, NewOrder.class);
+            i.putExtra("table", table);
             startActivity(i);
         }
     };
--- CruddyPizza/app/src/main/java/com/example/cruddypizza/NewOrder.java
+++ CruddyPizza/app/src/main/java/com/example/cruddypizza/NewOrder.java
@@ -3,58 +3,178 @@ package com.example.cruddypizza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TableLayout;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class NewOrder extends AppCompatActivity {
-    String size;
+    int size = 0, numToppings = 0;
     RadioButton btn_small, btn_med, btn_lrg, btn_xlrg;
+    CheckBox chk_topping1, chk_topping2, chk_topping3, chk_topping4, chk_topping5, chk_topping6, chk_topping7, chk_topping8, chk_topping9, chk_topping10, chk_topping11, chk_topping12, chk_topping13, chk_topping14;
+    EditText editText_firstName, editText_lastName, editText_address, editText_phone;
+    Button btn_submit;
+    TableLayout toppingSection;
+    Intent intent;
+    ArrayList<String> order = new ArrayList<>();
+    ArrayList<ArrayList<String>> table = new ArrayList<>();
+    ArrayList<CheckBox> toppings = new ArrayList<>();
+    Animation shake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_order);
+        shake = AnimationUtils.loadAnimation(NewOrder.this, R.anim.shake);
 
         btn_small = findViewById(R.id.btn_small);
         btn_med = findViewById(R.id.btn_medium);
         btn_lrg = findViewById(R.id.btn_large);
         btn_xlrg = findViewById(R.id.btn_xlarge);
 
+        toppingSection = findViewById(R.id.toppingSection);
+        chk_topping1 = findViewById(R.id.chk_topping1);
+        toppings.add(chk_topping1);
+        chk_topping2 = findViewById(R.id.chk_topping2);
+        toppings.add(chk_topping2);
+        chk_topping3 = findViewById(R.id.chk_topping3);
+        toppings.add(chk_topping3);
+        chk_topping4 = findViewById(R.id.chk_topping4);
+        toppings.add(chk_topping4);
+        chk_topping5 = findViewById(R.id.chk_topping5);
+        toppings.add(chk_topping5);
+        chk_topping6 = findViewById(R.id.chk_topping6);
+        toppings.add(chk_topping6);
+        chk_topping7 = findViewById(R.id.chk_topping7);
+        toppings.add(chk_topping7);
+        chk_topping8 = findViewById(R.id.chk_topping8);
+        toppings.add(chk_topping8);
+        chk_topping9 = findViewById(R.id.chk_topping9);
+        toppings.add(chk_topping9);
+        chk_topping10 = findViewById(R.id.chk_topping10);
+        toppings.add(chk_topping10);
+        chk_topping11 = findViewById(R.id.chk_topping11);
+        toppings.add(chk_topping11);
+        chk_topping12 = findViewById(R.id.chk_topping12);
+        toppings.add(chk_topping12);
+        chk_topping13 = findViewById(R.id.chk_topping13);
+        toppings.add(chk_topping13);
+        chk_topping14 = findViewById(R.id.chk_topping14);
+        toppings.add(chk_topping14);
+
+
+        editText_firstName = findViewById(R.id.editTextFirstName);
+        editText_lastName = findViewById(R.id.editTextLastName);
+        editText_address = findViewById(R.id.editTextAddress);
+        editText_phone = findViewById(R.id.editTextPhone);
+
+        btn_submit = findViewById(R.id.btn_submitOrder);
+
         btn_small.setOnClickListener(onSizeButtonClicked);
+        btn_med.setOnClickListener(onSizeButtonClicked);
+        btn_lrg.setOnClickListener(onSizeButtonClicked);
+        btn_xlrg.setOnClickListener(onSizeButtonClicked);
+
+        for (CheckBox topping: toppings) {
+            topping.setOnClickListener(onToppingClicked);
+        }
+
+        btn_submit.setOnClickListener(onNewClicked);
+
+        intent = getIntent();
+        if (intent != null) {
+            table = (ArrayList<ArrayList<String>>) intent.getSerializableExtra("table");
+            if (table == null) {
+                table = new ArrayList<>();
+            }
+        }
     }
 
     public View.OnClickListener onSizeButtonClicked = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-//            switch (view.getId()) {
-//                case R.id.btn_small: {
-//                    size = "Small";
-//                }
-//
-//                case R.id.btn_medium: {
-//
-//                }
-//
-//                case R.id.btn_large: {
-//
-//                }
-//
-//                case R.id.btn_xlarge: {
-//
-//                }
-//            }
-            size  = ((RadioButton)view).getText().toString();
             btn_small.setChecked(false);
             btn_med.setChecked(false);
             btn_lrg.setChecked(false);
             btn_xlrg.setChecked(false);
             ((RadioButton) view).setChecked(true);
+
+        }
+    };
+
+    public View.OnClickListener onToppingClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (numToppings < 3 && ((CheckBox) view).isChecked()) {
+                numToppings += 1;
+            } else if (!((CheckBox) view).isChecked()) {
+                numToppings -=1;
+            } else {
+                view.startAnimation(shake);
+                ((CheckBox) view).setChecked(false);
+            }
+        }
+    };
+
+    public View.OnClickListener onNewClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            boolean isGood = true;
+            if (numToppings == 0) {
+                toppingSection.startAnimation(shake);
+                isGood = false;
+            }
+            if (editText_firstName.getText().toString().length() == 0) {
+                editText_firstName.startAnimation(shake);
+                isGood = false;
+            }
+            if (editText_lastName.getText().toString().length() == 0) {
+                editText_lastName.startAnimation(shake);
+                isGood = false;
+            }
+            if (editText_address.getText().toString().length() == 0) {
+                editText_address.startAnimation(shake);
+                isGood = false;
+            }
+            if (editText_phone.getText().toString().length() == 0) {
+                editText_phone.startAnimation(shake);
+                isGood = false;
+            }
+            if (btn_small.isChecked()) {
+                size = 1;
+            } else if (btn_med.isChecked()) {
+                size = 2;
+            } else if (btn_lrg.isChecked()) {
+                size = 3;
+            } else if (btn_xlrg.isChecked()) {
+                size = 4;
+            }
+            if (isGood) {
+                order.add(editText_firstName.getText().toString());
+                order.add(editText_lastName.getText().toString());
+                order.add(editText_address.getText().toString());
+                order.add(editText_phone.getText().toString());
+                order.add(String.valueOf(size));
+                for (CheckBox topping: toppings) {
+                    if (topping.isChecked()) {
+                        order.add(topping.getText().toString());
+                    }
+                }
+                table.add(order);
+                Intent i = new Intent(NewOrder.this, MainActivity.class);
+                i.putExtra("table", table);
+                startActivity(i);
+            }
         }
     };
 }
